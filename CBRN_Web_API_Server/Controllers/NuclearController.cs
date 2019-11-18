6e62b3032baab71d739f49de3c6206ea8a9dcf6e
Claudiu-George Purice <PuriceL.Claudiu@gmail.com>
2019-11-18 10:15:53 +0200

    make server app

--- CBRN_Web_API_Server/Controllers/NuclearController.cs
+++ CBRN_Web_API_Server/Controllers/NuclearController.cs
@@ -9,14 +9,15 @@ using CBRN_Web_API_Server.Models;
 using CBRN_Project.MVVM.Models.Chemical;
 using CBRN_Project.Data_Access;
 
+
 namespace CBRN_Web_API_Server.Controllers
 {
-    public class ReportController : ApiController
+    public class NuclearController : ApiController
     {
+        private IEnumerable<KeyValuePair<int, DailyReport>> NuclearWrapper(Input i) { return new KeyValuePair<int, DailyReport>[0]; }
         public IEnumerable<KeyValuePair<int, DailyReport>> GetReport([FromBody] Input input)
         {
-            ChemModel model = new ChemModel(new DataService(), input.MethParams, input.Icons);
-            return model.MakeReport();
+            return NuclearWrapper(input);
         }
     }
 }
---
+++ CBRN_Web_API_Server/Controllers/RadiologicalController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using CBRN_Project.MVVM.Models;
+using CBRN_Web_API_Server.Models;
+using CBRN_Project.MVVM.Models.Chemical;
+using CBRN_Project.Data_Access;
+
+
+
+namespace CBRN_Web_API_Server.Controllers
+{
+    public class RadiologicalController : ApiController
+    {
+        private IEnumerable<KeyValuePair<int, DailyReport>> RadiologicalWrapper(Input i) { return new KeyValuePair<int, DailyReport>[0]; }
+        public IEnumerable<KeyValuePair<int, DailyReport>> GetReport([FromBody] Input input)
+        {
+            return RadiologicalWrapper(input);
+        }
+    }
+}
